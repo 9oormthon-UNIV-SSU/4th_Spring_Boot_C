bfcdbdc5606ef38a9a8d3e3eed2c3ab3b0517345
나용준 <141994188+youngJun99@users.noreply.github.com>
2025-04-03 19:47:00 +0900

    초기 세팅

---
+++ src/main/java/study/spring_boot_c/SpringBootCApplication.java
@@ -0,0 +1,13 @@
+package study.spring_boot_c;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCApplication.class, args);
+	}
+
+}
