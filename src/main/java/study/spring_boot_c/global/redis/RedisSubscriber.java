f585996ef31e09c512724da3f726dc6721cc8b8b
Ssamssamukja <109636635+Ssamssamukja@users.noreply.github.com>
2025-05-28 12:27:32 +0900

    refactor: redisSubscriber 수정

--- src/main/java/study/spring_boot_c/global/redis/RedisSubscriber.java
+++ src/main/java/study/spring_boot_c/global/redis/RedisSubscriber.java
@@ -3,6 +3,7 @@ package study.spring_boot_c.global.redis;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.connection.MessageListener;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
 import study.spring_boot_c.domain.chat.converter.ChatMessageConverter;
@@ -15,7 +16,7 @@ import java.time.LocalDateTime;
 
 @Component
 @RequiredArgsConstructor
-public class RedisSubscriber {
+public class RedisSubscriber implements MessageListener {
 
     private final SimpMessagingTemplate messagingTemplate;
     private final ChatMessageRepository chatMessageRepository;
@@ -28,9 +29,6 @@ public class RedisSubscriber {
 
             // 1. WebSocket으로 클라이언트에게 전송
             messagingTemplate.convertAndSend("/topic/chatroom/" + chatMessage.getRoomId(), chatMessage);
-
-            // 2. MongoDB 저장
-            chatMessageRepository.save(ChatMessageConverter.toChatMessage(chatMessage));
 
         } catch (Exception e) {
             e.printStackTrace();
