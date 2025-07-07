f3392e463a6df17dbe64d8e36e738c36f1d55e4f
wnd01jun <changjun157@naver.com>
2025-07-07 18:18:21 +0900

    [chore] MemberRepository email기반 JWT 작성으로 findByEmail 추가

--- src/main/java/study/spring_boot_c/domain/member/domain/repository/MemberRepository.java
+++ src/main/java/study/spring_boot_c/domain/member/domain/repository/MemberRepository.java
@@ -3,5 +3,9 @@ package study.spring_boot_c.domain.member.domain.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import study.spring_boot_c.domain.member.domain.entity.Member;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member,Long> {
+
+    Optional<Member> findByEmail(String email);
 }
